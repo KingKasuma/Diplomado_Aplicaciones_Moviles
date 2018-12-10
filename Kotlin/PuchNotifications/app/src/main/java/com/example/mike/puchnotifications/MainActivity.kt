8521aae3c0d607735fef3fc833a4fefd506498f3
Mike <lanselot777hailander@gmail.com>
2018-12-10 19:53:05 -0400

    some changes

---
+++ Kotlin/PuchNotifications/app/src/main/java/com/example/mike/puchnotifications/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.mike.puchnotifications
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
