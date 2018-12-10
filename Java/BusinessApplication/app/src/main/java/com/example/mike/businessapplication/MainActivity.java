354165bf24d13476b544f6154e6ac8ebae1694a7
Mike <lanselot777hailander@gmail.com>
2018-12-10 11:57:18 -0400

    App of bussines

---
+++ Java/BusinessApplication/app/src/main/java/com/example/mike/businessapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.mike.businessapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeButton(View view){
+        String id = view.getTag().toString();
+        //Integer id = view.getId();
+
+        Log.i("Id tag",id);
+
+        TextView textView = findViewById(R.id.textView);
+
+        switch (id){
+            case "3":
+                Button button3 = findViewById(R.id.button3);
+                textView.setText(button3.getText());
+                break;
+            case "2":
+                Button button2 = findViewById(R.id.button2);
+                textView.setText(button2.getText());
+                break;
+            case "1":
+                Button button = findViewById(R.id.button);
+                textView.setText(button.getText());
+                break;
+            case "4":
+                Button button4 = findViewById(R.id.button4);
+                textView.setText(button4.getText());
+                break;
+            case "5":
+                Button button5 = findViewById(R.id.button5);
+                textView.setText(button5.getText());
+                break;
+        }
+    }
+}
